fdc029d3c1db84d81911ae00745117dde9fc854e
WifeSwap <pgabl@yahoo.com>
2020-01-16 15:37:05 -0600

    finished project

--- src/guessinggame/GuessingGame.java
+++ src/guessinggame/GuessingGame.java
@@ -9,6 +9,8 @@ package guessinggame;
  *
  * @author jonathan.gabl
  */
+import java.util.Scanner;
+
 public class GuessingGame {
 
     /**
@@ -16,6 +18,46 @@ public class GuessingGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        String[] food = {"pizza", "burger", "pasta"};
+        String randomFood = food[(int) (Math.random() * 3)];
+        String[] guesses = new String[3];
+        String currentGuess = "";
+        boolean playing = true;
+        boolean isFood = false;
+        int currentIndex = 0;
+        System.out.println("The secret meal must be one of the following");
+        for (int i = 0; i < food.length; i++) {
+            System.out.println(food[i]);
+        }
+        System.out.println(randomFood);
+        while (playing) {
+            System.out.print("Guess the secret meal:");
+            currentGuess = input.nextLine().toLowerCase();
+            for (int i = 0; i < food.length; i++) {
+                if (currentGuess.equals(food[i])) {
+                    isFood = true;
+                }
+            }
+            if (currentGuess.equals(randomFood)) {
+                System.out.println("You Win");
+                playing = false;
+            } else {
+                if (isFood) {
+                    for (int i = 0; i < food.length; i++) {
+                        if (currentGuess.equals(guesses[i])) {
+                            System.out.println("You Already Guessed that, You Lose!");
+                            playing = false;
+                        }
+                    }
+                    guesses[currentIndex] = currentGuess;                    
+                    currentIndex++;
+                } else {
+                    System.out.println("You Lose!");
+                    playing = false;
+                }
+            }
+            isFood = false;
+        }
     }
-    
 }
